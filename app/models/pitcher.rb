f4d852eea609593d7f8db09a5dde88ec35927c61
tanizawa_kenta <kt10025102@gmail.com>
2019-12-04 11:24:34 +0900

    add:lテストコード

--- app/models/pitcher.rb
+++ app/models/pitcher.rb
@@ -1,2 +1,8 @@
 class Pitcher < ApplicationRecord
+  def self.most_winners_in_term(start_year, end_year)
+    pitchers = Pitcher.where(year_id: start_year..end_year)
+    max_win = pitchers.maximum(:wins)
+    best_pitchers = pitchers.where(wins: max_win)
+    best_pitchers.pluck(:player_id)
+  end
 end
